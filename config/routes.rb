2dc94dcf54a239105b5544127aacdd872a272060
Michelle Sun <michesun@gmail.com>
2012-03-03 12:24:47 +0800

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 SampleApp::Application.routes.draw do
+    
+  get "users/new"
+  
+  match '/signup', :to => 'users#new'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  root :to => 'pages#home'
+  
   get "pages/michellepage"
 
   get "pages/home"
@@ -6,17 +15,6 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
   
   get "pages/about"
-  
-  
-SampleApp::Application.routes.draw do
-    
-  match '/contact', :to => 'pages#contact'
-  
-  match '/about', :to => 'pages#about'
-  
-  match '/help', :to => 'pages#help'
-  
-  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -74,5 +72,5 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
 end
